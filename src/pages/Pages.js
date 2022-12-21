2c63455a64f9b5cc64fc6c2838633643783bdeac
Bhaskar p <pbhaskar870@gmail.com>
2022-12-21 13:38:08 +0530

    adding all the pages except a search

---
+++ src/pages/Pages.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Home from './Home';
+import{Route, Routes} from 'react-router-dom';
+import Cuisine from './Cuisine';
+import Searched from './Searched';
+
+const Pages = () => {
+  return (
+  
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Cuisine/:type' element={<Cuisine />} />
+          <Route path='/Searched/:search' element={<Searched />} />
+      </Routes>
+
+  )
+}
+
+export default Pages
---
+++ src/pages/Recipe.js
@@ -0,0 +1,10 @@
+import style from 'styled-components';
+import React,{useState,useEffect} from 'react'
+
+const Recipe = () => {
+  return (
+    <div>Recipe</div>
+  )
+}
+
+export default Recipe
